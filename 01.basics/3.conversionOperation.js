14c3332c8e8f0b53868275193c54fbe51f6ee58e
joinwithabhi1608 <119477882+joinwithabhi1608@users.noreply.github.com>
2023-08-27 11:18:08 +0000

    Conversion of datatypes

---
+++ 01.basics/3.conversionOperation.js
@@ -0,0 +1,23 @@
+
+let score = "33abc" 
+
+console.log(typeof score );
+console.log(typeof (score) );
+
+let valueInNumber = Number(score) ;
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+/* 1=> true ; 0=>false */
+let isloggedin = "abhijeet"
+let boolean = Boolean(isloggedin);
+console.log(boolean);
+
+/* "" => false
+   "Abhijeet" => true  */  
+
+   let somenumber = 33 
+   let stringnumber = String(somenumber) 
+   console.log(stringnumber);
+   console.log(typeof stringnumber);
+
