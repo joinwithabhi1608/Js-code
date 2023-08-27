7fb39f6375c7ce87942531d5e88757c5fdcf50fa
joinwithabhi1608 <119477882+joinwithabhi1608@users.noreply.github.com>
2023-08-27 10:38:05 +0000

    datatypes

---
+++ 01.basics/2.datatype.js
@@ -0,0 +1,30 @@
+"use strict" ;
+/* treat all js code as newer version */
+
+/* we are using nodejs */
+ // code readiability is important
+
+
+
+
+let name = "Abhijeet" ;
+
+let age = 18 ;
+
+let isLoggedIn = false ;
+
+/* number => 2^53
+bigint 
+string 
+boolean 
+null == undefined or empty
+symbol
+
+
+object 
+
+*/
+console.log( typeof age);
+
+
+
